204a8c30a33fa84158448ab1987fbd2c994360a0
unknown <User@EZ153.gou.go.ug>
2023-12-07 00:05:12 +0300

    Transactions Layout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.dashboard.home');
 });
+
+Route::get('/transactions', function () {
+    return view('pages.dashboard.transactions');
+});
